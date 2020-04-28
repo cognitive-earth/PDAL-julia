027fbbb413e8d4b81b269fee3444c62fc20bf0e8
Julian Fell <hi@jtfell.com>
2020-04-28 14:18:49 +1000

    install package compiler in build script

--- build_sys.jl
+++ build_sys.jl
@@ -1,7 +1,8 @@
-using PackageCompiler
 using Pkg
 
+Pkg.add("PackageCompiler")
 Pkg.add("TypedTables")
 
+using PackageCompiler
 create_sysimage(:TypedTables, sysimage_path="pdal_jl_sys.so", precompile_execution_file="jl/Import.jl")
 
