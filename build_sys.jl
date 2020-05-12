664259bd2690ac05c6fa1c5a4d34521ac6cf0043
Julian Fell <hi@jtfell.com>
2020-05-12 16:36:49 +1000

    fix sysimage build script

--- build_sys.jl
+++ build_sys.jl
@@ -8,5 +8,8 @@ Pkg.add("StructArrays")
 Pkg.add("StaticArrays")
 
 using PackageCompiler
-create_sysimage(:TypedTables, sysimage_path="pdal/pdal_jl_sys.so", precompile_execution_file="jl/Import.jl")
+
+packages = [:TypedTables, :RoamesGeometry, :AcceleratedArrays, :StructArrays, :StaticArrays]
+
+create_sysimage(packages, sysimage_path="pdal/pdal_jl_sys.so", precompile_execution_file="jl/Import.jl")
 
